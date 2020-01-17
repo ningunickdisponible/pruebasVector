e142afcbea49eff13284994a578b2cecabfbaa03
Bruno <brunob@seincomp.com.ar>
2020-01-16 21:50:15 -0300

    subida a git

---
+++ PruebasConVector/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace PruebasConVector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVectormayor = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGauss = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnVectormayor
+            // 
+            this.btnVectormayor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVectormayor.Location = new System.Drawing.Point(44, 12);
+            this.btnVectormayor.Name = "btnVectormayor";
+            this.btnVectormayor.Size = new System.Drawing.Size(107, 53);
+            this.btnVectormayor.TabIndex = 0;
+            this.btnVectormayor.Text = "mayor valor del vector";
+            this.btnVectormayor.UseVisualStyleBackColor = true;
+            this.btnVectormayor.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(201, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // btnGauss
+            // 
+            this.btnGauss.Location = new System.Drawing.Point(44, 116);
+            this.btnGauss.Name = "btnGauss";
+            this.btnGauss.Size = new System.Drawing.Size(107, 46);
+            this.btnGauss.TabIndex = 2;
+            this.btnGauss.Text = "Suma de gauss faltante";
+            this.btnGauss.UseVisualStyleBackColor = true;
+            this.btnGauss.Click += new System.EventHandler(this.BtnGauss_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Bubble sort";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(201, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 312);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnGauss);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnVectormayor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVectormayor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGauss;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PruebasConVector/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebasConVector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int[] vec = randomVector();
+            int mayor = 0;
+            for (int x = 0; x < vec.Length; x++)
+            {
+                Console.WriteLine(vec[x]);
+                if (mayor < vec[x])
+                {
+                    mayor = vec[x];
+                }
+            }
+            label1.Text = mayor.ToString();     
+        }
+
+        public int[] randomVector()
+        {
+            Random random = new Random();
+            int size = random.Next(0, 20);
+            int[] res = new int[size];
+            for (int x = 0; x < size; x++)
+            {
+                res[x] = random.Next(0, 50);
+            }
+            return res;
+        }
+
+        public int[] randomVectorFaltanteUno()
+        {
+            Random random = new Random();
+            int faltante = random.Next(0, 100000);
+            int[] res = new int[100000];
+            for (int x = 0; x < 100000; x++)
+            {
+                if (x >= faltante) {
+                    res[x] = x + 1;
+                }
+                else
+                {
+                    res[x] = x;
+                }
+            }
+            //shuffle
+            for (int t = 0; t< res.Length; t++)
+            {
+                int tmp = res[t];
+                int r = random.Next(t, res.Length);
+                res[t] = res[r];
+                res[r] = tmp;
+            } 
+            return res;
+        }
+
+        private void BtnGauss_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            int[] vecFaltanteUno = randomVectorFaltanteUno();
+            int Gauss = (vecFaltanteUno.Length / 2) * (vecFaltanteUno.Length + 1);
+            
+            int suma = 0;
+            for (int x = 0; x < vecFaltanteUno.Length; x++)
+            {
+                suma = suma + vecFaltanteUno[x];
+            }
+            Cursor.Current = Cursors.Default;
+            label2.Text = (Gauss - suma).ToString();
+        }
+        void bubbleSort(ref int[] arr)
+        {
+            int temp = 0;
+            for (int write = 0; write < arr.Length; write++)
+            {
+                for (int sort = 0; sort < arr.Length - 1; sort++)
+                {
+                    if (arr[sort] > arr[sort + 1])
+                    {
+                        temp = arr[sort + 1];
+                        arr[sort + 1] = arr[sort];
+                        arr[sort] = temp;
+                    }
+                }
+            }
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            int[] vecFaltanteUno = randomVectorFaltanteUno();
+            bubbleSort(ref vecFaltanteUno);
+            Cursor.Current = Cursors.Default;
+        }
+    }
+}
